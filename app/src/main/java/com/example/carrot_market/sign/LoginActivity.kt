eddfdf4b15de9689d662d7526fe05e6dbfa97641
rdridrid <rdridrid@naver.com>
2023-07-18 00:37:25 +0900

    login 파트

--- app/src/main/java/com/example/carrot_market/sign/LoginActivity.kt
+++ app/src/main/java/com/example/carrot_market/sign/LoginActivity.kt
@@ -3,10 +3,15 @@ package com.example.carrot_market.sign
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.carrot_market.R
+import com.example.carrot_market.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val binding = ActivityLoginBinding.inflate(layoutInflater)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+
     }
+
 }
